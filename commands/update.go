5ea4db7531a1b9bbaae006ddafe2809cfa5c2399
Charles Oliveira <charles.oliveira@linaro.org>
2021-05-17 10:45:16 -0300

    commands: update: add update command
    Update lets users iterate over added vidx files
    and retrieve list of packages.
    
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

---
+++ commands/update.go
@@ -0,0 +1,27 @@
+package commands
+
+
+import (
+    "fmt"
+    "github.com/spf13/cobra"
+    "github.com/chaws/cmpack/packs"
+    "os"
+)
+
+
+//
+//  Command: vidx
+//
+//  Should be followed be "add", "rm" or "list"
+//
+var UpdateCmd = &cobra.Command{
+    Use:   "update",
+    Short: "Update the list of packs",
+    Args: cobra.ExactArgs(0),
+    Run: func(cmd *cobra.Command, args []string) {
+        if err := packs.UpdateList(); err != nil {
+            fmt.Fprintf(os.Stderr, "E: %s\n", err)
+            os.Exit(-1)
+        }
+    },
+}
