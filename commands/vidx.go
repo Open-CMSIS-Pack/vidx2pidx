6db9ff0d57c9130bab54bb01c915d23dfa9a4288
Charles Oliveira <charles.oliveira@linaro.org>
2021-05-17 03:21:21 -0300

    commands: vidx: add `vidx add` command
    This will allow users to add multiple vidx source
    
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

--- commands/vidx.go
+++ commands/vidx.go
@@ -1,16 +1,48 @@
 package commands
 
-import "fmt"
 
-var Vidx = struct {
-    Add func(name, source string) int
-}{
-    Add: add,
+import (
+    "fmt"
+    "github.com/spf13/cobra"
+    "github.com/chaws/cmpack/config"
+    "os"
+)
+
+
+//
+//  Command: vidx
+//
+//  Should be followed be "add", "rm" or "list"
+//
+var VidxCmd = &cobra.Command{
+    Use:   "vidx",
+    Short: "Add, list or remove vendor index sources",
+    Args: cobra.ExactArgs(1),
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Fprintf(os.Stderr, "Uknown command '%s'.", args[0])
+    },
 }
 
-func add(name, source string) int {
-    fmt.Println("Adding from vidx")
-    fmt.Println("Name ", name)
-    fmt.Println("Source ", source)
-    return 0
+
+//
+//  Command: vidx add <vidx-name> <vidx-path>
+//
+//  Adds a vidx source to cmpack config file
+//
+var vidxAddCmd = &cobra.Command{
+    Use: "add <name> <path>",
+    Args: cobra.ExactArgs(2),
+    Run: func(cmd *cobra.Command, args []string) {
+        var name = args[0]
+        var path = args[1]
+        if err := config.AddVidx(name, path); err != nil {
+            fmt.Fprintf(os.Stderr, "E: %s\n", err)
+            os.Exit(-1)
+        }
+    },
+}
+
+
+func init() {
+    VidxCmd.AddCommand(vidxAddCmd)
 }
