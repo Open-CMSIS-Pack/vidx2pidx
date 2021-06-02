bae56a1ccda8999f4d5913e9b5e048d5692b2acc
Charles Oliveira <charles.oliveira@linaro.org>
2021-06-02 11:35:15 -0300

    utils_test: add support for monkey patching
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/open-cmsis-pack/vidx2pidx
 go 1.16
 
 require (
+	bou.ke/monkey v1.0.2 // indirect
 	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
 	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/ugorji/go v1.1.4 // indirect
--- utils_test.go
+++ utils_test.go
@@ -3,9 +3,11 @@ package main
 import (
 	"testing"
 	"errors"
+	"os"
+	"bou.ke/monkey"
 )
 
-func TestAnyErr(t *testing.T){
+func TestAnyErr(t *testing.T) {
 	var errs []error
 	if AnyErr(errs) != nil {
 		t.Errorf("AnyErr should return nothing when empty is given")
@@ -14,12 +16,32 @@ func TestAnyErr(t *testing.T){
 	foo := errors.New("Foo error")
 	errs = append(errs, foo)
 	if AnyErr(errs) != foo {
-		t.Errorf("AnyErr should return the first error in the array of errors")
+		t.Error("AnyErr should return the first error in the array of errors")
 	}
 
 	bar := errors.New("Bar error")
 	errs = append(errs, bar)
 	if AnyErr(errs) != foo {
-		t.Errorf("AnyErr should return the first error in the array of errors")
+		t.Error("AnyErr should return the first error in the array of errors")
+	}
+}
+
+func TestExitOnError(t *testing.T) {
+	var exitCode = 0
+	monkey.Patch(os.Exit, func(code int) {
+		exitCode = code
+	})
+
+	ExitOnError(nil)
+
+	if exitCode != 0 {
+		t.Error("ExitOnError should not exit when no error is given")
+	}
+
+	foo := errors.New("Foo error")
+	ExitOnError(foo)
+
+	if exitCode != -1 {
+		t.Error("ExitOnError should exit when error is given")
 	}
 }
