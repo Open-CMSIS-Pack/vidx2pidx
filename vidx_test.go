5e6d713a7ba8e01baa1a62b345726839e0103f41
Charles Oliveira <charles.oliveira@linaro.org>
2021-06-04 17:42:06 -0300

    vidx_test: add basic tests for vidx.go
    The VidxXML is quite simple, and testing other parts of the
    code already test this a lot. Will add more if needed.
    
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

---
+++ vidx_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"testing"
+
+	"bou.ke/monkey"
+)
+
+func TestVidx(t *testing.T) {
+	t.Run("test fail to read xml", func(t *testing.T) {
+		errMessage := "failed to read XML"
+		monkey.Patch(ReadXML, func(string, interface{}) error {
+			return errors.New(errMessage)
+		})
+
+		vidx := NewVidx()
+		err := vidx.Init("test/dummy.xml")
+		if err == nil {
+			t.Error("VidxXML.Init() should fail if XML cannot be read")
+		}
+
+		AssertEqual(t, err.Error(), errMessage)
+
+		monkey.Unpatch(ReadXML)
+	})
+}
